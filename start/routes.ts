9e797cb7030051092c91f0106e94406409bceb43
Keegan Fontaine <keegsfontaine@gmail.com>
2024-10-25 20:46:29 -0400

    a

--- start/routes.ts
+++ start/routes.ts
@@ -9,7 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 router.post('/test', async () => {
-  return 'hello from post'
+  return    'hello from post'
 })
 
 router.get('/test', async () => {
