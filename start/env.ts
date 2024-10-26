afddd6707c92675776720a7aad6ca5d085beda90
Keegan Fontaine <keegsfontaine@gmail.com>
2024-10-25 20:30:03 -0400

    testing formatter

--- start/env.ts
+++ start/env.ts
@@ -12,24 +12,24 @@
 import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
-  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
-  APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
+      NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+      PORT: Env.schema.number(),
+      APP_KEY: Env.schema.string(),
+      HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.string(),
 
   /*
-  |----------------------------------------------------------
-  | Variables for configuring session package
-  |----------------------------------------------------------
-  */
+                                |----------------------------------------------------------
+                                | Variables for configuring session package
+                                |----------------------------------------------------------
+                                */
   SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
 
   /*
-  |----------------------------------------------------------
-  | Variables for configuring database connection
-  |----------------------------------------------------------
-  */
+                                |----------------------------------------------------------
+                                | Variables for configuring database connection
+                                |----------------------------------------------------------
+                                */
   DB_HOST: Env.schema.string({ format: 'host' }),
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
