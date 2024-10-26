f7565d4275679f8362d3f88fa763298ce6da203f
Keegan Fontaine <keegsfontaine@gmail.com>
2024-10-25 20:14:40 -0400

    Initial commit

---
+++ inertia/pages/hello.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let user: { name: string }
+</script>
+
+<h1>
+  <p>Hello {user.name}</p>
+</h1>
---
+++ start/routes.ts
@@ -0,0 +1,17 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+router.post('/test', async () => {
+  return 'hello from post'
+})
+
+router.get('/test', async () => {
+  return 'hi'
+})
